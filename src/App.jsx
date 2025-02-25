58326803b0159e211d4aa129c34ce3da0071b25e
Rajat Srivastava <rsrajatsri@gmail.com>
2025-02-26 00:05:50 +0530

    cart page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Cart from "./features/cart/Cart";
 function App() {
 
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/cart" element={<Cart />} />
         {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
   );
--- src/features/navbar/Navbar.jsx
+++ src/features/navbar/Navbar.jsx
@@ -13,6 +13,7 @@ import {
   ShoppingCartIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 
 export default function Navbar({ children }) {
   const user = {
@@ -73,14 +74,16 @@ export default function Navbar({ children }) {
             </div>
             <div className="hidden md:block">
               <div className="ml-4 flex items-center md:ml-6">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <ShoppingCartIcon aria-hidden="true" className="size-6" />
-                </button>
+                <Link to="/cart" className="cursor-pointer">
+                  <button
+                    type="button"
+                    className="relative  rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
+                  >
+                    <span className="absolute -inset-1.5" />
+                    <span className="sr-only">View notifications</span>
+                    <ShoppingCartIcon aria-hidden="true" className="size-6" />
+                  </button>
+                </Link>
                 <span className=" inline-flex items-center rounded-md mb-7 -ml-3 relative bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-red-600/10 ring-inset">
                   3
                 </span>
@@ -170,14 +173,16 @@ export default function Navbar({ children }) {
                   {user.email}
                 </div>
               </div>
-              <button
-                type="button"
-                className="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
-              >
-                <span className="absolute -inset-1.5" />
-                <span className="sr-only">View notifications</span>
-                <ShoppingCartIcon aria-hidden="true" className="size-6" />
-              </button>
+              <Link to="/cart" className="cursor-pointer">
+                <button
+                  type="button"
+                  className="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
+                >
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">View notifications</span>
+                  <ShoppingCartIcon aria-hidden="true" className="size-6" />
+                </button>
+              </Link>
               <span className="inline-flex items-center rounded-md mb-7 -ml-3 relative bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-red-600/10 ring-inset">
                 3
               </span>
